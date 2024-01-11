9e1015bfe49a210d264963e06db95c7d92ff2ed0
Lucas Curtin <lucas.curtin@gmail.com>
2024-01-11 16:20:15 +0000

    EFN is still tensorflow based! compiling is good idea

--- script.py
+++ script.py
@@ -23,6 +23,13 @@ def main(config: config_loader.Config) -> None:
         train_path=test_path,
         test_path=test_path,
     )
+    model.compile(
+        optimizer="adam",
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"],
+        weighted_metrics=["accuracy"],
+    )
+
     logger.info("Training Model")
 
     batch_size = config.batch_size
